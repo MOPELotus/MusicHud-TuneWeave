dcbb4a662635bf7b084c9fcfadabe6c69b6b1ea1
MOPELotus <2402804043@qq.com>
2026-09-14 06:33:52 +0800

    Add CF distribution builds and document local TuneWeave setup

---
+++ core/src/standard/java/indi/mopelotus/musichud/BuildDistribution.java
@@ -0,0 +1,9 @@
+package indi.mopelotus.musichud;
+
+/** Defaults selected by Gradle, never by a runtime flag. */
+public final class BuildDistribution {
+    public static final boolean DEFAULT_AUTO_START = true;
+    public static final String DEFAULT_EXECUTABLE_PATH = "musichud-tuneweave/tuneweave";
+
+    private BuildDistribution() {}
+}
